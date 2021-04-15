fb88551271c7e90551e457a683ef30eb8c79fbe7
yan <2466541844@qq.com>
2021-04-15 11:14:22 +0800

    mybatis-test1

--- 03_MyBatis/src/com/study/util/ServiceFactory.java
+++ 03_MyBatis/src/com/study/util/ServiceFactory.java
@@ -7,7 +7,7 @@ package com.study.util;
 public class ServiceFactory {
 
 
-    //传递张三对象（目标对象），得到李四对象（代理对象）的过程
+    //传递工厂对象（目标对象），得到淘宝对象（代理对象）的过程
 
     public static Object getService(Object service){
 
